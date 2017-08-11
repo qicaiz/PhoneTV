bd1e6cd223005b8d04c861dcf7e4e8e371cfb466
qicaiz <zhouqicai415@163.com>
2017-08-11 22:58:43 +0800

    添加主页面注释

--- app/src/main/java/qc/com/phonetv/MainActivity.java
+++ app/src/main/java/qc/com/phonetv/MainActivity.java
@@ -3,14 +3,14 @@ package qc.com.phonetv;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 主页面
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-        //
+        setContentView(R.layout.activity_main);
     }
 }
